61a3c1704a85fae98c4cd0599eeed8d18a509267
Manh-Ha Hoang <manhha.hoang@neura-robotics.com>
2024-07-10 10:51:11 +0200

    add training script

---
+++ scripts/train.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="/home/manhhahoang/data/fabric_defect_detection.v1i.yolov8/data.yaml", epochs=300)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
+results = model("/home/manhhahoang/data/fabric_defect_detection.v1i.yolov8/test/images/z5549753700613_e29abad32aa9304c32fdea6f45750c78_jpg.rf.72abc507ab6951327aaf72e85f9468c3.jpg")  # predict on an image
+path = model.export(format="onnx")  # export the model to ONNX format
