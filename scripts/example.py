6c90dbd61ecaa11b19f1afd87d52eeb90b6dac37
manhha <manhha.hoang@neura-robotics>
2024-09-18 11:38:54 +0700

    update

--- scripts/example.py
+++ scripts/example.py
@@ -3,12 +3,12 @@ import tensorrt_inference_py
 from pathlib import Path
 img_file = Path.home() / "data" / "test_images" / "image2.png"
 image =cv2.imread(img_file.as_posix())
-detector = tensorrt_inference_py.detection.Detection("retinaface2","")
+detector = tensorrt_inference_py.detection.Detection("facedetector","")
 params = tensorrt_inference_py.detection.DetectionParams()
 rect = tensorrt_inference_py.detection.Rect()
 
 
-objects = detector.detect(image,params)
+objects = detector.detect(image,params,['face'])
 print(len(objects))
-image1 = detector.draw(image,objects,params,["face"],2)
+image1 = detector.draw(image,objects,params,2)
 cv2.imwrite("result.png",image1)
