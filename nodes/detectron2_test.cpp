c8cf42691af381cc753afcb9c3196f878b011804
manhhahoang <20105284+manhha1402@users.noreply.github.com>
2024-12-26 10:26:31 +0100

    update

--- nodes/detectron2_test.cpp
+++ nodes/detectron2_test.cpp
@@ -1,18 +1,18 @@
 #include <yaml-cpp/yaml.h>
 
-
 #include "tensorrt_inference/tensorrt_inference.h"
 // Runs object detection on video stream then displays annotated results.
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Read the input image
   std::string inputImage = argv[1];
   std::string model_name = argv[2];
   auto img = cv::imread(inputImage);
-  if (img.empty()) {
+  if (img.empty())
+  {
     std::cout << "Error: Unable to read image at path '" << inputImage << "'"
               << std::endl;
     return -1;
   }
- tensorrt_inference::Model model(model_name);
- 
+  tensorrt_inference::Model model(model_name);
 }
